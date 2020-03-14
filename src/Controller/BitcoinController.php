e003d354244069d7ebb293799fed09907a728be9
mori <morteza_faraji@yahoo.com>
2020-03-13 17:43:34 -0700

    bitcoin controller

--- src/Controller/BitcoinController.php
+++ src/Controller/BitcoinController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 use App\Service\BitcoinService;
 
@@ -19,4 +20,20 @@ class BitcoinController extends AbstractController
             'bitcoin_wallet_info' => $bitcoinService->getWalletInfo()
         ]);
     }
+    
+    /**
+     * @Route("/admin/bitcoin/address", name="admin-bitcoin-tx")
+     */
+    public function getAddress(BitcoinService $bitcoinService, Request $request)
+    {
+        $tx = $request->query->get('tx');
+        echo $tx;
+        $wallet_address = $bitcoinService->getAddressByTransaction($tx);
+        echo $bitcoinService->displayError();
+        
+        return $this->render('bitcoin/tx.html.twig', [
+            'controller_name' => 'BitcoinController',
+            'tx' => $wallet_address
+        ]);
+    }       
 }
